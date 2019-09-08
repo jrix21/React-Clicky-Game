27cd81c087c990b754247f80e003c63203cd26d1
Pheorix <jrix21@gmail.com>
2019-09-08 00:02:04 -0500

    Added pseudocode to unfinished (app.js)and cleanup

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -13,12 +13,35 @@ class App extends Component {
     highscore: 0
   };
 
+  //Randomize Array
+      /*maybe I need to add a "position" variable to the Friend object so that I can change that value with math.random
+      
+      */
+
+  //Scoring
+      /*Create array for CLicked images 
+      /store value of images ID
+      if that value is the same as previous value
+      (Loss) user has clicked same image twice
+      (Win) The ID's are never the same twice 
+            increent score
+      */
+
+ //High Score Updater
+      /*If score > highscore
+      set score to highscore
+      Manipulate the state
+      */
+
+  /*Left this bit in to use as a refernce in altering the state
+  Removing because its non essential to the game
+
   removeFriend = id => {
     // Filter this.state.friends for friends with an id not equal to the id being removed
     const friends = this.state.friends.filter(friend => friend.id !== id);
     // Set this.state.friends equal to the new friends array
     this.setState({ friends });
-  };
+  };*/
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
